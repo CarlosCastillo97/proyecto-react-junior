1ebdfdefed5518922e441aa976bae90cbcb20698
Carlos <carloscastillo1997@hotmail.es>
2026-08-02 11:38:38 -0600

    Agregando boton tema claro y estilizando CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import {useState} from "react";
 import Contador from "./components/Contador";
 import ListaTareas from "./components/ListaTareas";
 import BuscadorAnime from "./components/BuscadorAnime";
 import "./index.css";
 
 function App() {
+const [temaClaro, setTemaClaro] = useState(false);
+
   return (
-    <div>
+    <div className={temaClaro ? "tema-claro" : ""}>
+      <button onClick={() => setTemaClaro(!temaClaro)}>
+        Cambiar tema
+      </button>
+      
       <h1>Mi Proyecto React</h1>
 
       <div className="bloque">
